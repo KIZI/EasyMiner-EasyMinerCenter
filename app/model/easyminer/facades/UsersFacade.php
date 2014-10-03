fef79f7ee289ce188943628e4d37712eb275c9c1
Stanislav Vojíř <stanislav.vojir@gmail.com>
2014-10-03 15:39:31 +0200

    přihlašování uživatelů

--- app/model/easyminer/facades/UsersFacade.php
+++ app/model/easyminer/facades/UsersFacade.php
@@ -7,7 +7,9 @@ use App\Model\EasyMiner\Entities\User;
 use App\Model\EasyMiner\Repositories\UsersRepository;
 use Nette\Security\AuthenticationException;
 use Nette\Security\IAuthenticator;
+use Nette\Security\Identity;
 use Nette\Security\IIdentity;
+use Nette\Security\Passwords;
 use Nette\Utils\DateTime;
 use Nette\Utils\Strings;
 
@@ -64,17 +66,68 @@ class UsersFacade implements IAuthenticator{
   }
 
   /**
-   * @param $googleUserId
-   * @param $googleProfile
+   * @param string $googleUserId
+   * @param object $googleProfile
+   * @throws AuthenticationException
    * @return User
    */
   public function authenticateUserFromGoogle($googleUserId,$googleProfile){
-    //TODO
+    $googleProfile->email=Strings::lower($googleProfile->email);
+    $user=null;
+    try{
+      //zkusíme najít existujícího uživatele
+      $user=$this->findUserByGoogleId($googleUserId);
+      if ($user){
+        //pokud máme uživatele dle facebookId, musíme zkontrolovat, jestli tu není kolize v mailech... (jestli nemáme uživatele registrovaného samostatně, který by měl zadaný stejný mail)
+        if ($user->email!=$googleProfile->email){
+          try{
+            $userByMail=$this->findUserByEmail($googleProfile->email);
+          }catch (\Exception $e){/*chyba nás nezajímá*/}
+          if (isset($userByMail) && ($userByMail instanceof User)){
+            //sloučení uživatelských účtů
+            $user=$this->mergeUsers($userByMail,$user);
+          }else{
+            if ($user->email!=$googleProfile->email){
+              $user->email=$googleProfile->email;
+            }
+          }
+        }
+      }else{
+        //pokud ho zatím nemáme připárovaného, zkusíme uživatele najít podle mailu
+        $user=$this->findUserByEmail($googleProfile->email);
+      }
+    }catch (\Exception $e){/*chybu ignorujeme (dala se čekat)*/}
+
+    if (!$user){
+      //registrace nového uživatele
+      $user=new User();
+      $user->email=$googleProfile->email;
+      $user->googleId=$googleUserId;
+      $user->name=$googleProfile->givenName.' '.$googleProfile->familyName;
+      $user->active=true;
+      $user->lastLogin=new DateTime();
+      $this->usersRepository->persist($user);
+    }else{
+      //kontrola, jestli není uživatel zablokován
+      if (!$user->active){
+        throw new AuthenticationException('User account is blocked.',self::NOT_APPROVED);
+      }
+      //update info o stávajícím uživateli
+      if ($user->name!=$googleProfile->givenName.' '.$googleProfile->familyName){
+        $user->name=$googleProfile->givenName.' '.$googleProfile->familyName;;
+      }
+      $user->lastLogin=new DateTime();
+      $this->usersRepository->persist($user);
+    }
+    //stáhnutí obrázku
+    $this->saveUserImageFromUrl($user,$googleProfile->picture);
+    return $this->getUserIdentity($user);
   }
 
   /**
-   * @param $facebookUserId
-   * @param $facebookMe
+   * @param string $facebookUserId
+   * @param object $facebookMe
+   * @throws AuthenticationException
    * @return User
    */
   public function authenticateUserFromFacebook($facebookUserId,$facebookMe){
@@ -83,7 +136,22 @@ class UsersFacade implements IAuthenticator{
     try{
       //zkusíme najít existujícího uživatele
       $user=$this->findUserByFacebookId($facebookUserId);
-      if (!$user){
+      if ($user){
+        //pokud máme uživatele dle facebookId, musíme zkontrolovat, jestli tu není kolize v mailech... (jestli nemáme uživatele registrovaného samostatně, který by měl zadaný stejný mail)
+        if ($user->email!=$facebookMe->email){
+          try{
+            $userByMail=$this->findUserByEmail($facebookMe->email);
+          }catch (\Exception $e){/*chyba nás nezajímá*/}
+          if (isset($userByMail) && ($userByMail instanceof User)){
+            //sloučení uživatelských účtů
+            $user=$this->mergeUsers($userByMail,$user);
+          }else{
+            if ($user->email!=$facebookMe->email){
+              $user->email=$facebookMe->email;
+            }
+          }
+        }
+      }else{
         //pokud ho zatím nemáme připárovaného, zkusíme uživatele najít podle mailu
         $user=$this->findUserByEmail($facebookMe->email);
       }
@@ -104,33 +172,86 @@ class UsersFacade implements IAuthenticator{
         throw new AuthenticationException('User account is blocked.',self::NOT_APPROVED);
       }
       //update info o stávajícím uživateli
-      if ($user->email!=$facebookMe->email){
-        $user->email=$facebookMe->email;
+      if ($user->name!=$facebookMe->first_name.' '.$facebookMe->last_name){
+        $user->name=$facebookMe->first_name.' '.$facebookMe->last_name;
       }
+      $user->lastLogin=new DateTime();
+      $this->usersRepository->persist($user);
     }
-
-
-
-
-
-
+    //stáhnutí obrázku
+    $this->saveUserImageFromUrl($user,'https://graph.facebook.com/'.$facebookUserId.'/picture?width=400&height=400');
+    return $this->getUserIdentity($user);
   }
 
 
   /**
+   * @param User|int $user
+   * @param string $url
+   */
+  private function saveUserImageFromUrl($user,$url){
+    //TODO dodělat stáhnutí obrázku a uložení na serveru (pro další použití)
+  }
+
+  /**
+   * @param User $user
+   * @param User $duplicateUser
+   * @return User
+   */
+  public function mergeUsers(User $user, User $duplicateUser){
+    //TODO not implemented! mělo by dojít ke sloučení uživatelského účtu z facebooku či googlu pod hlavní uživatelský účet
+    return $duplicateUser;
+  }
+
+  /**
    *
-   * @return IIdentity
+   * @param array $credentials
    * @throws AuthenticationException
+   * @return IIdentity
    */
   function authenticate(array $credentials) {
-    // TODO: Implement authenticate() method.
+    list($email, $password) = $credentials;
+    $email=Strings::lower($email);
+    try{
+      $user=$this->findUserByEmail($email);
+    }catch (\Exception $e){
+      throw new AuthenticationException('User account was not found.', self::IDENTITY_NOT_FOUND,$e);
+    }
+    if (!$user->active){
+      throw new AuthenticationException('User account is blocked.', self::NOT_APPROVED);
+    }
+    if (!Passwords::verify($password,$user->password)){
+      throw new AuthenticationException('Invalid combination of email and password.', self::IDENTITY_NOT_FOUND);
+    }
+    return $this->getUserIdentity($user);
   }
 
   /**
    * @param User|int $user
-   * @return IIdentity
+   * @return Identity
+   * @throws AuthenticationException
+   * @throws \Exception
    */
   public function getUserIdentity($user){
-    //TODO
+    if (!($user instanceof User)){
+      try{
+        $user=$this->usersRepository->find($user);
+      }catch (\Exception $e){
+        throw new AuthenticationException('User account was not found.', self::IDENTITY_NOT_FOUND,$e);
+      }
+    }
+    if (!$user->active){
+      throw new AuthenticationException('User account is blocked.', self::INVALID_CREDENTIAL);
+    }
+
+    $rolesArr=array('authenticated');
+    /*TODO uživatelské role
+    $userRoles=$user->roles;
+    if (!empty($userRoles)){
+      foreach ($userRoles as $userRole){
+        $rolesArr[]=$userRole->roleId;
+      }
+    }
+    */
+    return new Identity($user->userId,$rolesArr,array('name'=>$user->name,'email'=>$user->email));
   }
 }
