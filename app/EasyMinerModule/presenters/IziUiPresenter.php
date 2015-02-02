2736e6abb331a23f55390f0395a7d47655c0ef82
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-02-02 23:43:57 +0100

    feature list dle typu mineru
    #17

--- app/EasyMinerModule/presenters/IziUiPresenter.php
+++ app/EasyMinerModule/presenters/IziUiPresenter.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\EasyMinerModule\Presenters;
 
+use App\Model\EasyMiner\Entities\Miner;
 use App\Model\EasyMiner\Facades\DatasourcesFacade;
 use IZI\IZIConfig;
 use IZI\Parser\DataParser;
 use Nette\Application\ForbiddenRequestException;
+use Nette\Utils\Strings;
 
 /**
  * Class IziUiPresenter - prezenter obsahující funkcionalitu vyžadovanou javascriptovým uživatelským rozhraním (migrace PHP kódu z projektu EasyMiner2)
@@ -27,28 +29,23 @@ class IziUiPresenter extends BasePresenter{
     if (empty($miner)){
       $miner=$id_dm;
     }
-    if ($id_dm=='TEST'){
-      $DP = new DataParser($this->config->DDPath, $this->config->FLPath, $this->config->FGCPath, null, null, $this->lang);
-      $DP->loadData();
-      $responseContent = $DP->parseData();
-      $responseContent['status'] = 'ok';
-      $this->sendJsonResponse($responseContent);
-      return;
-    }
 
     //------------------------------------------------------------------------------------------------------------------
+    $minerType=Miner::DEFAULT_TYPE;
     if ($miner){
       $miner=$this->minersFacade->findMiner($miner);
       if (!$this->minersFacade->checkMinerAccess($miner,$this->user->id)){
         throw new ForbiddenRequestException($this->translator->translate('You are not authorized to access selected miner data!'));
       }
+      $minerType=$miner->type;
     }
+    $FLPathElement='FLPath_'.Strings::upper($minerType);
 
     //------------------------------------------------------------------------------------------------------------------
     #region připravení informací pro UI - s odděleným připravením DataDictionary
     //TODO připravení DataDescriptionPMML
     $dataDescriptionPMML=null;
-    $dataParser = new DataParser($dataDescriptionPMML, $this->config->FLPath, $this->config->FGCPath, null, null, $this->lang);//TODO kontrola, jestli může být obsah předán bez uložení do souboru
+    $dataParser = new DataParser($dataDescriptionPMML, $this->config->$FLPathElement, $this->config->FGCPath, null, null, $this->lang);//TODO kontrola, jestli může být obsah předán bez uložení do souboru
     $dataParser->loadData();
     $responseContent = $dataParser->parseData();
 
@@ -64,6 +61,7 @@ class IziUiPresenter extends BasePresenter{
 
 
     $responseContent['status'] = 'ok';
+    $responseContent['miner_type'] = $miner->type;
 
     $this->sendJsonResponse($responseContent);
   }
