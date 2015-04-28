0bb5dba57ce4ca4206bc263cdab655c4e2f702d0
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-04-29 00:16:59 +0200

    rozdělení bootstrapu na normální a na variantu s použitím restful api (kvůli rychlosti běhu)

---
+++ app/restBootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new Nette\Configurator;
+
+$configurator->setDebugMode(true);
+$configurator->enableDebugger(__DIR__ . '/../log');
+
+$configurator->setTempDirectory(__DIR__ . '/../temp/rest');
+
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__)
+	->addDirectory(__DIR__ . '/../vendor/others')
+	->register();
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+$configurator->addConfig(__DIR__ . '/config/izi-ui.config.neon');
+
+  $configurator->addConfig(__DIR__ . '/config/restful.config.neon');
+
+$container = $configurator->createContainer();
+
+return $container;
--- www/index.php
+++ www/index.php
@@ -3,6 +3,10 @@
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require '.maintenance.php';
 
-$container = require __DIR__ . '/../app/bootstrap.php';
-
-$container->getService('application')->run();
+if (strpos($_SERVER['REQUEST_URI'],'/rest/') && !strpos($_SERVER['REQUEST_URI'],'/rest/auth')) {
+  $container = require __DIR__ . '/../app/restBootstrap.php';
+  $container->getService('application')->run();
+}else{
+  $container = require __DIR__ . '/../app/bootstrap.php';
+  $container->getService('application')->run();
+}
