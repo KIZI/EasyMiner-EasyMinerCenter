a8e53ea96753c2f8ad1de60432c705f47adac0c0
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-07-02 23:01:33 +0200

    zabalení javascriptu pro HeaderMenu do objektu, ošetření skrytí submenu při kliknutí na ikonu nápovědy
    issue #18

--- www/js/main.js
+++ www/js/main.js
@@ -1,97 +1,133 @@
-
-/*region headerMenu - menu zobrazovaná v hlavičce*/
 /**
- * Funkce pro nastavení absolutní pozice pro menu v hlavičce (absolutně vzhledem k dokumentu)
- * @param menuBlock
- * @param menuLink
- * @param documentWidth
+ * Object for management of HeaderMenu
+ * @param options
+ * @constructor
  */
-var setHeaderMenuPosition = function (menuBlock, menuLink,documentWidth){
-  var SPACE_FROM_RIGHT=10;
-  var MENU_LINK_ARROW_SIZE=9;
-  var menuLinkCenterFromDocumentLeft = menuLink.offset().left+Math.floor(menuLink.outerWidth()/2);
-  var menuLinkCenterFromDocumentRight = documentWidth-menuLink.offset().left-Math.floor(menuLink.outerWidth()/2)-SPACE_FROM_RIGHT;
-  var menuBlockWidth = menuBlock.outerWidth();
-  var menuBlockShiftCenterToLeft=Math.max(0, Math.ceil(menuBlockWidth/2)-menuLinkCenterFromDocumentRight);
-  var menuBlockLeft=(menuLinkCenterFromDocumentLeft-menuBlockShiftCenterToLeft-Math.ceil(menuBlockWidth/2));
-  menuBlock.css({left:menuBlockLeft,top:(menuLink.offset().top+menuLink.outerHeight()+MENU_LINK_ARROW_SIZE)});
-  var menuLinkArrowLeft = Math.floor((menuBlockWidth-MENU_LINK_ARROW_SIZE)/2)+menuBlockShiftCenterToLeft;
-  menuBlock.find('div.menuLinkArrow').css({left:menuLinkArrowLeft});
-};
+var HeaderMenu = function(options){
+  var $ = jQuery.noConflict();
+  this.menuVisible = false;
+  /**
+   * @type {{subMenus: Array, links: Array, SPACE_FROM_RIGHT: number, MENU_LINK_ARROW_SIZE: number}}
+   */
+  this.options = {
+    subMenus : ['User','Apps'],
+    links : ['Help'],
+    SPACE_FROM_RIGHT : 10,
+    MENU_LINK_ARROW_SIZE : 9
+  };
 
-/**
- * Připojení událostí nápovědy po načtení stránky
- */
-$(document).ready(function($){
-  $('.headerMenu').append('<div class="menuLinkArrow"></div>');
-  var menuVisible=false;
-  var headerUserLink = $('#headerUserLink');
-  if (headerUserLink){
-    /**
-     * Skrytí všech headerMenu při kliknutí na jiný element
-     */
+  /**
+   *
+   * Funkce pro nastavení absolutní pozice pro menu v hlavičce (absolutně vzhledem k dokumentu)
+   * @param {object} menuBlock
+   * @param {object} menuLink
+   * @private
+   */
+  this.setSubMenuPosition = function(menuBlock, menuLink){
+    var documentWidth = $(document).width();
+    var menuLinkCenterFromDocumentLeft = menuLink.offset().left+Math.floor(menuLink.outerWidth()/2);
+    var menuLinkCenterFromDocumentRight = documentWidth-menuLink.offset().left-Math.floor(menuLink.outerWidth()/2)-this.options.SPACE_FROM_RIGHT;
+    var menuBlockWidth = menuBlock.outerWidth();
+    var menuBlockShiftCenterToLeft=Math.max(0, Math.ceil(menuBlockWidth/2)-menuLinkCenterFromDocumentRight);
+    var menuBlockLeft=(menuLinkCenterFromDocumentLeft-menuBlockShiftCenterToLeft-Math.ceil(menuBlockWidth/2));
+    menuBlock.css({left:menuBlockLeft,top:(menuLink.offset().top+menuLink.outerHeight()+this.options.MENU_LINK_ARROW_SIZE)});
+    var menuLinkArrowLeft = Math.floor((menuBlockWidth-this.options.MENU_LINK_ARROW_SIZE)/2)+menuBlockShiftCenterToLeft;
+    menuBlock.find('div.menuLinkArrow').css({left:menuLinkArrowLeft});
+  };
+
+  /**
+   * Funkce pro inicializaci jednotlivých subMenu
+   * @param {string} menuItem
+   * @private
+   */
+  this.initSubMenu = function(menuItem){
+    var linkElement = $('#header'+menuItem+'Link');
+    if (linkElement){
+      var headerMenu = this;
+      linkElement.click(function(event){
+        if ($(this).hasClass('active')){
+          //skryjeme menu
+          headerMenu.menuVisible=false;
+          $('.headerMenu').hide();
+          $('.headerLinks a').removeClass('active');
+        }else{
+          //zobrazíme menu
+          $('.headerMenu').hide();
+          headerMenu.menuVisible=false;
+          $('.headerLinks a').removeClass('active');
+          var menuElement=$('#header'+menuItem+'Menu');
+          if (menuElement){
+            headerMenu.setSubMenuPosition(menuElement, linkElement);
+            headerMenu.menuVisible=true;
+            menuElement.show();
+            $(this).addClass('active');
+          }
+        }
+        event.stopPropagation();
+        return false;
+      });
+    }
+  };
+
+  /**
+   * Funkce pro inicializaci jednotlivých odkazů bez submenu
+   * @param {string} menuItem
+   * @private
+   */
+  this.initLink = function(menuItem){
+    var linkElement = $('#header'+menuItem+'Link');
+    if (linkElement){
+      linkElement.click(function(){
+        //skryjeme menu
+        this.menuVisible=false;
+        $('.headerMenu').hide();
+        $('.headerLinks a').removeClass('active');
+      }.bind(this));
+    }
+  };
+
+  /**
+   * @private
+   * @param options
+   */
+  this.init = function(options){
+    $.extend(this.options, options);
+    /*region připojení menuLinkArrow k jednotlivým submenu*/
+    $('.headerMenu').append('<div class="menuLinkArrow"></div>');
+    /*endregion*/
+    /*region skrytí všech headerMenu při kliknutí na jiný element */
     $(document).mousedown(function(event){
-      if (!menuVisible){return;}
+      if (!this.menuVisible){return;}
       var targetElement=$(event.target);
       if (!(targetElement.hasClass('headerMenu') || targetElement.closest('.headerMenu').length || targetElement.closest('.headerLinks').length)){
         $('.headerMenu').hide();
         $('.headerLinks a').removeClass('active');
       }
-    });
-    /**
-     * Událost při kliknutí na odkaz v hlavičce (zobrazení/skrytí menu)
-     */
-    headerUserLink.click(function(event){
-      if ($(this).hasClass('active')){
-        //skryjeme menu
-        menuVisible=false;
-        $('.headerMenu').hide();
-        $('.headerLinks a').removeClass('active');
-      }else{
-        //zobrazíme menu
-        $('.headerMenu').hide();
-        menuVisible=false;
-        $('.headerLinks a').removeClass('active');
-        var headerUserMenu=$('#headerUserMenu');
-        if (headerUserMenu){
-          setHeaderMenuPosition(headerUserMenu,headerUserLink,$(document).width());
-          menuVisible=true;
-          headerUserMenu.show();
-          $(this).addClass('active');
-        }
-      }
-      event.stopPropagation();
-      return false;
-    });
-  }
+    }.bind(this));
+    /*endregion*/
+    /*region inicializace jednotlivých submenu*/
+    $.each(this.options.subMenus,function(index, value){
+      this.initSubMenu(value);
+    }.bind(this));
+    /*endregion*/
+    /*region inicializace nápovědy*/
+    $.each(this.options.links,function(index, value){
+      this.initLink(value);
+    }.bind(this));
+    /*endregion*/
+  };
 
-  var headerAppsLink = $('#headerAppsLink');
-  if (headerAppsLink){
-    headerAppsLink.click(function(event){
-      if ($(this).hasClass('active')){
-        //skryjeme menu
-        menuVisible=false;
-        $('.headerMenu').hide();
-        $('.headerLinks a').removeClass('active');
-      }else{
-        //zobrazíme menu
-        menuVisible=false;
-        $('.headerMenu').hide();
-        $('.headerLinks a').removeClass('active');
+  /*initialize object*/
+  this.init($,options);
+};
 
-        var headerAppsMenu=$('#headerAppsMenu');
-        if (headerAppsMenu){
-          setHeaderMenuPosition(headerAppsMenu,headerAppsLink,$(document).width());
-          menuVisible=true;
-          headerAppsMenu.show();
-          $(this).addClass('active');
-        }
-      }
-      event.stopPropagation();
-      return false;
-    });
-  }
 
+/*region headerMenu - menu zobrazovaná v hlavičce*/
+/**
+ * Připojení událostí nápovědy po načtení stránky
+ */
+$(document).ready(function($){
+  new HeaderMenu($);
 });
 
 /*endregion headerMenu*/
