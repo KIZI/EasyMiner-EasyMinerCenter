cc42cef0f6009acffc59d82a701fd49a21360019
Stanislav Vojíř <stanislav.vojir@gmail.com>
2018-08-28 00:33:40 +0200

    práce na integraci BRE s EasyMinerCenter

--- app/EasyMinerModule/presenters/BrePresenter.php
+++ app/EasyMinerModule/presenters/BrePresenter.php
@@ -2,6 +2,7 @@
 namespace EasyMinerCenter\EasyMinerModule\Presenters;
 
 use EasyMiner\BRE\Integration as BREIntegration;
+use EasyMinerCenter\Model\EasyMiner\Facades\RuleSetsFacade;
 
 /**
  * Class BrePresenter - presenter with the functionality for integration of the submodule EasyMiner-BRE
@@ -10,16 +11,27 @@ use EasyMiner\BRE\Integration as BREIntegration;
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  */
 class BrePresenter extends BasePresenter{
+  /** @var RuleSetsFacade $ruleSetsFacade */
+  private $ruleSetsFacade;
 
   /**
    * Action for display of EasyMiner-BRE
+   * @param int $ruleset
    */
-  public function renderDefault(){
+  public function renderDefault($ruleset){
     $this->template->javascriptFiles=BREIntegration::$javascriptFiles;
     $this->template->cssFiles=BREIntegration::$cssFiles;
     $this->template->content=BREIntegration::getContent();
     $this->template->moduleName=BREIntegration::MODULE_NAME;
-    $this->template->GET=json_encode($_GET);
+    $this->template->ruleSet=$this->ruleSetsFacade->findRuleSet($ruleset);//TODO ošetření chyby
   }
 
+  #region injections
+  /**
+   * @param RuleSetsFacade $ruleSetsFacade
+   */
+  public function injectRuleSetsFacade(RuleSetsFacade $ruleSetsFacade){
+    $this->ruleSetsFacade=$ruleSetsFacade;
+  }
+  #endregion injections
 } 
